5b6988a4fe69cfc8f5e0e4d1d7ed9a660bc55b96
Firzar Ayupov <a.firzar.r@gmail.com>
2020-06-04 12:12:52 +0300

    Выполнил домашнее задание из 2 занятия

--- src/ru/ayupov/lesson02/task1/Main.java
+++ src/ru/ayupov/lesson02/task1/Main.java
@@ -2,8 +2,8 @@ package ru.ayupov.lesson02.task1;
 
 public class Main {
     public static void main(String[] args) {
-        int price = 41;
-        int volumes = 10;
+        int price = 43;
+        int volumes = 50;
         int sum = price * volumes;
         System.out.println("Цена 1 л. бензина - " + price + " руб.");
         System.out.println("Цена за " + volumes + " л. - " + sum + " руб.");
--- src/ru/ayupov/lesson02/task2/Main.java
+++ src/ru/ayupov/lesson02/task2/Main.java
@@ -2,11 +2,11 @@ package ru.ayupov.lesson02.task2;
 
 public class Main {
     public static void main(String[] args) {
-        double salary = 100000;
+        double salary = 70000;
         final int TAX = 13;
         double sum = salary - ((salary * TAX) / 100);
         System.out.println("Подоходный налог - " + TAX + "%");
         System.out.println("Зарплата до удержания налога - " + salary + " руб.");
-        System.out.println("Зарплата после удержания налога - " + sum + " руб.");
+        System.out.println("Зарплата после удержания налога, \"на руки\" - " + sum + " руб.");
     }
 }
